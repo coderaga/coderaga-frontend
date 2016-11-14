f0a306a09219b16ae80f3e7a74e3cec04210589c
voidzero <kartikjagdale11@gmail.com>
2016-11-14 11:17:08 +0530

    1. Integrated Creative Bootstrap theme 2. Seperated Theme into different components >> i.e header/contact/services/navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { NavbarComponent } from './navbar.component';
+import { AboutComponent } from './about.component';
+import { ServicesComponent } from './services.component';
+import { ContactComponent } from './contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
